d2a7a26f50310ebe3d3ccac1a3dcb79d720a0d58
Marcos Santos <marcossantosmsms604@gmail.com>
2024-04-18 20:06:08 -0300

    Criação da entidade user

---
+++ src/main/java/com/marcos/demoparkapi/entities/User.java
@@ -0,0 +1,131 @@
+package com.marcos.demoparkapi.entities;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "username", nullable = false, unique = true, length = 100)
+	private String username;
+	@Column(name = "password", nullable = false, length = 200)
+	private String password;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "role", nullable = false, length = 25)
+	private Role role;
+
+	@Column(name = "creation_date")
+	private LocalDateTime dataCriacao;
+	@Column(name = "modification_date")
+	private LocalDateTime dataModificacao;
+	@Column(name = "created_by")
+	private String criadoPor;
+	@Column(name = "modified_by")
+	private String modificadoPor;	
+	
+	public User() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDateTime dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public LocalDateTime getDataModificacao() {
+		return dataModificacao;
+	}
+
+	public void setDataModificacao(LocalDateTime dataModificacao) {
+		this.dataModificacao = dataModificacao;
+	}
+
+	public String getCriadoPor() {
+		return criadoPor;
+	}
+
+	public void setCriadoPor(String criadoPor) {
+		this.criadoPor = criadoPor;
+	}
+
+	public String getModificadoPor() {
+		return modificadoPor;
+	}
+
+	public void setModificadoPor(String modificadoPor) {
+		this.modificadoPor = modificadoPor;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	public enum Role {
+		ROLE_ADMIN, ROLE_CLIENTE
+	}
+}
