b810924b432ced3dc6f1db3c17265d3bc4459a3e
Marcos Santos <marcossantosmsms604@gmail.com>
2024-04-18 20:39:50 -0300

    Implementando o endpoint POST

--- src/main/java/com/marcos/demoparkapi/controller/UserController.java
+++ src/main/java/com/marcos/demoparkapi/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.marcos.demoparkapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.marcos.demoparkapi.entities.User;
 import com.marcos.demoparkapi.service.UserService;
 
 @RestController
@@ -12,5 +17,11 @@ public class UserController {
 	
 	@Autowired
 	private UserService userService;
+	
+	@PostMapping
+	public ResponseEntity<User> create(@RequestBody User user){
+		User userSave = userService.save(user);
+		return ResponseEntity.status(HttpStatus.CREATED).body(userSave);
+	}
 
 }
--- src/main/java/com/marcos/demoparkapi/service/UserService.java
+++ src/main/java/com/marcos/demoparkapi/service/UserService.java
@@ -3,12 +3,20 @@ package com.marcos.demoparkapi.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.marcos.demoparkapi.entities.User;
 import com.marcos.demoparkapi.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
+
+	@Transactional
+	public User save(User user) {
+		return userRepository.save(user);
+	}
 
 }
