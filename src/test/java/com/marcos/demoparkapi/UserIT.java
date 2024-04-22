95b649fbb571fc502a2c18645ed3bb64bf6bad85
Marcos Santos <marcossantosmsms604@gmail.com>
2024-04-22 17:57:15 -0300

    Inserindo testes no recurso post da classe usuario

---
+++ src/test/java/com/marcos/demoparkapi/UserIT.java
@@ -0,0 +1,38 @@
+package com.marcos.demoparkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.marcos.demoparkapi.dto.UserCreateDto;
+import com.marcos.demoparkapi.dto.UserResponseDto;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "users-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)//ANTES que metodo for executado é necessário que estaja no banco os registros
+@Sql(scripts = "users-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)// APOS o metodo ser executado que o delete irá ocorrer
+public class UserIT {
+	
+	 @Autowired
+	 WebTestClient webTestClient;
+	 
+	 @Test
+	    public void createUser_usernameAndPasswordValid_returnUserCreatedWithStatus201(){
+	     UserResponseDto  responseBody =  webTestClient
+	                .post()
+	                .uri("api/v1/usuarios")
+	                .contentType(MediaType.APPLICATION_JSON)
+	                .bodyValue(new UserCreateDto("anajulia@gmail.com", "123456"))
+	                .exchange()
+	                .expectStatus().isCreated()
+	                .expectBody(UserResponseDto.class)
+	                .returnResult().getResponseBody();
+	    org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+	    org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+	    org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("anajulia@gmail.com");
+	    org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENTE");
+	    }
+
+}
---
+++ src/test/java/com/marcos/demoparkapi/users-insert.sql
@@ -0,0 +1,3 @@
+insert into users (id, username, password, role) values (100, 'aluziagabriela@gmail.com', '12345782', 'ROLE_ADMIN');
+insert into users  (id, username, password, role) values (101, 'aluziagabrielaOne@gmail.com', '12345856', 'ROLE_ADMIN');
+insert into users (id, username, password, role) values (102, 'aluziagabrielaTwo@gmail.com', '12345212', 'ROLE_ADMIN');
