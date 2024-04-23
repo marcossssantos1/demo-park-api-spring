f5098cdebb3dd5940e42b9c28ebee552422faf49
Marcos Santos <marcossantosmsms604@gmail.com>
2024-04-22 22:10:36 -0300

    Adicionando uma classe para retornar o token do jwt

---
+++ src/main/java/com/marcos/demoparkapi/jwt/JwtToken.java
@@ -0,0 +1,25 @@
+package com.marcos.demoparkapi.jwt;
+
+public class JwtToken {
+	
+	private String token;
+
+	public JwtToken() {
+	}
+	
+	public JwtToken(String token) {
+		super();
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+
+}
